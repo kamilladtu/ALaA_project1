c16b9d6f5438683c5c9901b181f2bf383936839a
Bror Sigurd Bruland <s245414@dtu.dk>
2026-02-17 15:30:27 +0100

    added uv to project

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import ParameterSampler, RandomizedSearchCV, cross_val_score
 from scipy.stats import uniform
 import random
-np.random.seed(32)
-random.seed(32)
+from keras.datasets import mnist
+
+
+#loading mnist data:
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+
+
+# Shape:
+
+print('X_train: ' + str(train_X.shape))
+print('Y_train: ' + str(train_y.shape))
+print('X_test:  '  + str(test_X.shape))
+print('Y_test:  '  + str(test_y.shape))
